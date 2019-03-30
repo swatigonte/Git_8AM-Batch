8079044ec3e475b88d3c9a09283e1f0a1140160c
visalvani <vs.visalakshi@gmail.com>
2019-03-30 12:39:24 +0530

    Added the test case2

---
+++ src/org/test/Testcase2.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Testcase2 {
+	public static void main(String[] args) {
+		
+		System.out.println("Added the test case second");
+	}
+	public void Testmethod1()
+	{
+		System.out.println("add test method1");
+	}
+}
