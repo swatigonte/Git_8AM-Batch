ecb82c3852246f870f438afe1a7cc933b02e3b74
visalvani <vs.visalakshi@gmail.com>
2019-03-30 11:26:37 +0530

    create a cloning project

---
+++ src/org/test/Sample.java
@@ -0,0 +1,29 @@
+package org.test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+	
+				String s1="welcome to project class";
+				 String s2="GIT 8AM BATCH";
+				 
+		        System.out.println("The String1 is:" +s1);
+		        System.out.println(" The String2 is:" +s2);
+
+		        int len1=s1.length();
+		        System.out.println(len1);
+		    
+		        int len2=s2.length();
+		        System.out.println(len2);		        
+	}
+			
+}
+/*
+ The String1 is:welcome to project class
+ The String2 is:GIT 8AM BATCH
+24
+13
+ */
+	
+
+
